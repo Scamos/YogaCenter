e35cdb453fc427cf218d974037e8fb3c4b004ee8
Samuele Dario Scamozzi <scamosgame@gmail.com>
2025-07-05 16:30:26 +0200

    Test

---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="min-h-screen bg-slate-900 text-white p-5">
+    <h1 class="text-4xl font-bold text.cyan-400">Instruments</h1>
+    <form @submit.prevent="createInstrument" class="flex gap-2 mt-3">
+      <input type="text" v-model="instrumentEmoji" placeholder="Emoji" class="bg-slate-800 p-2 rounded-md text-white">
+      <input type="text" v-model="instrumentName" placeholder="Instrument name" class="bg-slate-800 p-2 rounded-md text-white">
+      <button type="submit" class="bg-cyan-500 text-white p-2 rounded-md">Add</button>
+    </form>
+    <ul class="list-disc ml-5 text-xl mt-4">
+      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument?.emoji }} {{ instrument.name }}<Icon name="mdi:trashcan" class="cursor-pointer hover:text-red-500" @click="deleteInstrument(instrument.id)"></Icon></li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const client = useSupabaseClient()
+const instruments = ref([])
+const instrumentName = ref('')
+const instrumentEmoji = ref('')
+const createInstrument = async () => {
+  const { data, error } = await client.from('instruments').insert({
+    name:instrumentName.value,
+    emoji:instrumentEmoji.value
+  }).select().single()
+  instruments.value.push(data)
+  if (error) throw error
+  instrumentName.value = ''
+  instrumentEmoji.value = ''  
+}
+const deleteInstrument = async (id) => {
+  instruments.value = instruments.value.filter(instrument => instrument.id !== id)
+  await client.from('instruments').delete().eq('id', id)
+}
+onMounted(async () => {
+  const { data, error } = await client.from('instruments').select()
+  instruments.value = data
+})
+</script>
